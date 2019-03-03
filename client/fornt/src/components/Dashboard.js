af6ad8626b75b82acf1eeab98e03f685d96e5728
Amila95 <amilawicramarathna95@gmail.com>
2019-03-03 13:25:26 +0530

    curdOperetionProject

--- client/fornt/src/components/Dashboard.js
+++ client/fornt/src/components/Dashboard.js
@@ -12,6 +12,10 @@ class Dashboard extends Component {
   }
 
   render() {
+
+    const {projects} = this.props.project
+
+
     return (
       <div className="projects">
       <div className="container">
@@ -22,7 +26,11 @@ class Dashboard extends Component {
                  <CreateProjectButton />
                   <br />
                   <hr />
-                  <ProjectItem />
+                  {
+                    projects.map(project => (
+                      <ProjectItem key={project.id} project={project} />
+                    ))
+                  }
 
               </div>
           </div>
